cb8be352ebb6011304efab5d639cfb1d6e50d54d
dmartyanov <d.martyanov@it.ecommpay.com>
2021-06-11 15:45:23 +0300

    Добавлена таблица с именами

--- bot.php
+++ bot.php
@@ -2,13 +2,16 @@
 // Подключение библиотеки
 use GuzzleHttp\Client;
 use Telegram\Api;
-include('conf.php');
+
+include('../bot/conf.php');
+
 
 class TelegramBot
 {
-	protected $updateId;
+    protected $token = "XXXXXXXXX";
+    protected $updateId;
 	// Функция собирает URL
-	protected function query($method, $params = [])
+	public function query($method, $params = [])
 	{
 		$url = "https://api.telegram.org/bot";
 		$url .= $this->token;
@@ -49,4 +52,75 @@ class TelegramBot
 		return $response;
 	}
 
+	// Принимает ноту и количество полутонов для транспонирования
+    public function transpose($chord, $transpose)
+    {
+        // the chords
+        $chords = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+        // get root tone
+        $root_arr = explode("/", $chord);
+        $root = strtoupper($root_arr[0]);
+
+        // the chord is the first character and a # if there is one
+        $root = $root[0] . ((strpos($root, "#") !== false) ? "#" : "");
+
+        // get any extra info
+        $root_extra_info = str_replace("#", "", substr($root_arr[0], 1)); // assuming that extra info does not have any #
+
+        // find the index on chords array
+        $root_index = array_search($root, $chords);
+        // transpose the values and modulo by 12 so we always point to existing indexes in our array
+        $root_transpose_index = floor(($root_index + $transpose) % 12);
+
+        if ($root_transpose_index < 0) {
+            $root_transpose_index += 12;
+        }
+
+        //$result .= $chords[$root_transpose_index] . $root_extra_info;
+
+        if (count($root_arr) > 1) {
+            // get the non root tone
+            $non_root = $root_arr[1];
+            // the chord is the first character and a # if there is one
+            $non_root = strtoupper($non_root[0]) . ((strpos($non_root, "#") !== false) ? "#" : "");
+            // get any extra info
+            $non_root_extra_info = str_replace("#", "", substr($root_arr[1], 1)); // assuming that extra info does not have any #
+
+            // find the index on chords array
+            $non_root_index = array_search($non_root, $chords);
+            // transpose the values and modulo by 12 so we always point to existing indexes in our array
+            $non_root_transpose_index = floor(($non_root_index + $transpose) % 12);
+
+            if ($non_root_transpose_index < 0) {
+                $non_root_transpose_index += 12;
+            }
+
+            $result .= "/" . $chords[$non_root_transpose_index] . $non_root_extra_info;
+        }
+
+        return $result;
+    }
+
+    public function schedule($text) {
+
+	    $names = explode(' ', $text, 50);
+        shuffle($names);
+        //$numOfNames = count($names);
+        //array_fill($numOfNames, 4, $names);
+        $table = "Июн| __1____8____15____22____29__|
+лдк | $names[0] $names[4] $names[8] $names[12] $names[16]
+прс | $names[1] $names[5] $names[9] $names[13] $names[17]
+слв | $names[2] $names[6] $names[10] $names[14] $names[18]
+двц | $names[3] $names[7] $names[11] $names[15] $names[19]";
+
+	    for ($numOfNames = count($names); $numOfNames < 20; $numOfNames++) {
+            //shuffle($names);
+
+            $names[] = array_fill($numOfNames, 1, $names[0]);
+	    }
+	    print_r($names);
+
+        return ($table);
+    }
 }
--- init.php
+++ init.php
@@ -3,6 +3,8 @@
 use GuzzleHttp\Client;
 include('vendor/autoload.php');
 include('bot.php');
+include('conf.php');
+//include ('tonality.php');
 
 //Получаем данные
 $telegramApi = new TelegramBot();
@@ -17,16 +19,21 @@ while (true) {
 			$text = $update->message->text; // Переменная с текстом сообщения
 			$chat_id = $update->message->chat->id; // Чат ID пользователя
 			$first_name = $update->message->chat->first_name; //Имя пользователя
+            $last_name = $update->message->chat->last_name; //Имя пользователя
 			$username = $update->message->chat->username; //Юзернейм пользователя
+            //$res = $telegramApi->transpose($text,3); //результат изменения пональности
+            $table = $telegramApi->schedule($update->message->text);
+
+            //$resOfReplace = str_ireplace($chords, $res, $text);
 
 			print_r($chat_id);
 			print_r($username);
 
 			if ($text == '/start'){ // Если пользователь подключился в первый раз, ему поступит приветствие
-			$telegramApi->sendMessage($chat_id, 'Привет'. ' ' . $first_name . '!'); //Приветствует Пользователя
+			    $telegramApi->sendMessage($chat_id, 'Привет'. ' ' . $first_name . '!'); //Приветствует Пользователя
 			} else {
-			$telegramApi->sendMessage($chat_id, $first_name . '! Как дела?' ); // Спрашивает как дела
-
+			    $telegramApi->sendMessage($chat_id, $table); // Спрашивает как дела
+                //echo $text;
 			}
 		}
 	}
